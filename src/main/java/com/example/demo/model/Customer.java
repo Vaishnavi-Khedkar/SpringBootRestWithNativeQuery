56bfe190726983e6b9d302adda86aa882541ee4a
vaishnavi <vgkhedkar05@gmail.com>
2022-09-19 18:19:17 +0530

    model class column name changed

--- src/main/java/com/example/demo/model/Customer.java
+++ src/main/java/com/example/demo/model/Customer.java
@@ -1,9 +1,12 @@
 package com.example.demo.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import org.hibernate.annotations.Columns;
 
 @Entity
 public class Customer {
@@ -12,6 +15,7 @@ public class Customer {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
+	@Column(name = "name")
 	private String name;
 	private String city;
 	private String mobile;
